789c8fb0d3376d8e7f08a83b66a7134185b8604d
Jarrett Drouillard <jarrett@thestyl.us>
2026-06-03 12:40:27 -0400

    chore(wezterm): add config files

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,50 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+local mux = wezterm.mux
+
+wezterm.on('gui-attached', function(domain)
+  local workspace = mux.get_active_workspace()
+
+  for _, window in ipairs(mux.all_windows()) do
+    if window:get_workspace() == workspace then
+      window:gui_window():maximize()
+    end
+  end
+end)
+
+local config = wezterm.config_builder()
+
+config.color_scheme = 'tokyonight'
+
+config.window_background_opacity = 0.5
+config.kde_window_background_blur = true
+
+config.font = wezterm.font 'FiraMono Nerd Font Mono'
+config.window_decorations = 'NONE'
+
+config.enable_tab_bar = false
+config.hide_tab_bar_if_only_one_tab = true
+
+config.keys = {
+  {
+    key = 'f',
+    mods = 'CMD|SHIFT',
+    action = wezterm.action.ToggleAlwaysOnTop,
+  },
+  {
+    key = 's',
+    mods = 'CTRL|SHIFT',
+    action = wezterm.action.SwitchToWorkspace {
+      name = 'scratch',
+    },
+  },
+  {
+    key = 'w',
+    mods = 'CTRL|SHIFT',
+    action = wezterm.action.ShowLauncherArgs {
+      flags = 'WORKSPACES',
+    },
+  },
+}
+
+return config
